858260ff7c541f37ab6da27217f3868e63d52632
Ramesh <rameshbarahate@gmail.com>
2018-06-19 20:11:31 +0530

    - design changes

---
+++ app/src/main/java/com/embibe/iibnanded/model/QuestionListModel.kt
@@ -0,0 +1,10 @@
+package com.embibe.iibnanded.model
+
+/**
+ * Created by mindstix on 18/06/18.
+ */
+
+class QuestionListModel(questionNo: Int, QuestionType: Int) {
+    internal var questionNo: Int = 0
+    internal var questionType: Int = 0
+}
